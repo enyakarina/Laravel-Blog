8d4a1f70dc76f6f29ae765ef7bc1da4b9824b187
Karina <karina@alisons-MacBook-Air.local>
2016-06-01 11:13:13 -0500

    Created a file to echo out the variables from the route.

---
+++ app/views/roll-dice.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Roll Dice</title>
+</head>
+<body>
+<h1>Enter a value in the link and see if you can guess the correct number!</h1>
+<p> You guessed <strong> <?php echo $guess ?> </strong>, and the correct number is <strong><?php echo $number ?></strong> </p>
+
+</body>
+</html>
