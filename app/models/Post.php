28cbbecbf1c18031cbf28a7c50f0ede23c66fae8
Karina <karina@alisons-MacBook-Air.local>
2016-06-06 15:58:26 -0500

    It now extends BaseModel. Added a new method that links it to a specific user.

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,5 +1,14 @@
 <?php
 
-class Post extends Eloquent {
+class Post extends BaseModel {
     protected $table = "posts";
+    public static $rules = array(
+    'title' => 'required|max:100',
+    'content' => 'required|max:10000'
+    );
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
 }
