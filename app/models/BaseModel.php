1275ae6c3f8fb3cb3aef1ccf15559b6ce7bde388
Karina <karina@alisons-MacBook-Air.local>
2016-06-06 15:55:41 -0500

    Model that contains 3 methods. One for displaying timezone in america/chicago, another one for hashing the password, and another to lowercase all usernames

---
+++ app/models/BaseModel.php
@@ -0,0 +1,21 @@
+<?php
+use Carbon\Carbon;
+class BaseModel extends Eloquent {
+    
+    public function getCreatedAtAttribute($value)
+    {
+        $utc = Carbon::createFromFormat($this->getDateFormat(), $value);
+        return $utc->setTimezone('America/Chicago');
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    public function setUsernameAttribute($value)
+    {
+        $this->attributes['username'] = strtolower($value);
+    }
+
+}
