365a272b994b43ec98cd7fe82af48bf1345e6b7e
Karina <karina@alisons-MacBook-Air.local>
2016-06-09 13:29:56 -0500

    Changed different headers.

--- app/views/profile/profile.blade.php
+++ app/views/profile/profile.blade.php
@@ -5,16 +5,18 @@ My Profile
 @stop
 
 @section('content')
-<div id ="container">
-    <h1>Profile</h1>
-    <h2>{{{ $user->username }}} <small>{{{ $user->profile->location }}}</h2>
-    <p><strong>Bio:</strong> {{{ $user->profile->bio }}}</p>
-    <h3>My posts:</h2>
+<div class="container" id="profileContainer">
+    <div class="page-header">
+        <h1><span class="username">{{{ $user->username }}} </span><small>Location: {{{ $user->profile->location }}}</small></h1>
+    </div>
+
+    <h2>Bio: </h2><p class="profile-description">{{{ $user->profile->bio }}}</p>
+    <h2>My posts:</h2>
         @foreach($user->posts as $post)
-        <p><a href="{{{action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></p>
+        <p class="profile-description"><a href="{{{action('PostsController@show', $post->id) }}}">{{{ $post->title }}}</a></p>
         @endforeach
     @if(Auth::user()->id == $user->id)
-        <h3><a href="{{{action('ProfilesController@edit', Auth::user()->username) }}}">Edit your profile</a></h3>
+        <a href="{{{action('ProfilesController@edit', $user->profile->id) }}}">Edit your profile</a>
     @endif
 </div>
 @stop
