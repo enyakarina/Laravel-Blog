822b3e263b6b0ec442d86e836b0aacd09a603c0d
Karina <karina@alisons-MacBook-Air.local>
2016-06-09 13:24:43 -0500

    Fixed the methods to create and edit the profile. Made it so that you cannot edit somebody else's information.

--- app/controllers/ProfilesController.php
+++ app/controllers/ProfilesController.php
@@ -6,10 +6,27 @@ class ProfilesController extends \BaseController
     public function showProfile($username)
     {
         $user = User::with('profile')->whereUsername($username)->first();
-        return View::make('profile.profile')->with('user', $user);
+
+        if($user->profile == null) {
+            return Redirect::action('ProfilesController@create', $user->username);
+        } else {
+            return View::make('profile.profile')->with('user', $user);
+        }
     }
 
-    public function edit()
+    public function edit($id) 
+    {
+        $profile = Profile::find($id);
+        $user = User::find($profile->user_id);
+        if (Auth::user()->id == $user->id) {
+            return View::make('profile.editprofile')->with('profile', $profile);
+        } else {
+            Session::flash('editError', 'Sorry, you cannot edit someone else\'s profile, but you can look at it!');
+            return Redirect::action('ProfilesController@showProfile', $user->username);
+        }
+    }
+
+    public function create()
     {
         return View::make('profile.createprofile');
     }
@@ -27,6 +44,24 @@ class ProfilesController extends \BaseController
             return Redirect::back()->withInput()->withErrors($validator);
         }else if($profile->save())
             Session::flash('profileSuccess', 'You have successfully updated your profile!');
-                return Redirect::to('/posts');
+                return Redirect::to('ProfilesController@showProfile');
+    }
+
+    public function update($id)
+    {
+        $profile = Profile::find($id);
+        $profile->location = Input::get('location');
+        $profile->bio = Input::get('bio');
+        $profile->user_id = Auth::user()->id;
+        $user = User::find($profile->user_id);
+        $validator = Validator::make(Input::all(), Profile::$rules);
+
+        if($validator->fails()) {
+            Session::flash('updateProfileError', 'Oops, there seems to be a problem updating your profile!');
+            return Redirect::back()->withInput()->withErrors($validator);
+        }else if($profile->save()) {
+            Session::flash('successMessage', 'You have successfully updated your profile!');
+            return Redirect::action('ProfilesController@showProfile', $user->username);
+        }
     }
 }
