3e93ca07758b8b409ce1f8a7fe2ed4422d3f5c95
Karina <karina@alisons-MacBook-Air.local>
2016-08-12 05:20:52 -0500

    edited homepage

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -7,18 +7,21 @@
 @section('content')
 
 <div class="container-fluid homepage-container">
-    <div class="page-header">
-        <h1 id="home-header">Hello, I'm Kari Palomino.</h1>
+    <div class="page-header col-lg-12">
+        <div class="col-lg-3">
+        <img src="/images/rainbow.svg">
+    </div>
+        <h1 id="home-header col-lg-8">Hello, I'm Enya Palomino.</h1>
         <h3 id="home-sub">Welcome to my page!</h3>
     </div>
     <div class="col-xs-12 col-md-6 col-lg-6" id="contentOne">
         <div class="thumbnail homepage-content">
-            <div class="caption">
+            <div class="caption home-boxes">
             <h2 class="home-h2">About Me</h2>
-            <p>I am a 22 year old person living in Texas who is  looking to transition into the world of web development. I started my coding journey fairly recently, in March 2016. Prior to this I had been attending community college while working part time in food service.</p> 
-
-            <p>
-            I learned to code by attending an intense 16-week program where I learned to build websites and web applications at a rapid pace. It has been one of the most rewarding things I have done for myself. I love to code, and I will continue learning and growing.</p>
+            <p>I am a 22 year old person living in Texas who is  looking to transition into the world of web development. I started my coding journey fairly recently, in March 2016. Prior to this I had been attending community college while working part time in food service.
+            <br>
+            <br>
+            I learned to code by attending an intense program where I learned to build websites and web applications at a rapid pace. It has been one of the most rewarding things I have done for myself. I love to code, and I will continue learning and growing.</p>
             </div>
         </div>
     </div>
@@ -26,8 +29,16 @@
     <div class="col-xs-12 col-md-6 col-lg-6" id="contentTwo">
         <div class="thumbnail homepage-content">
             <div class="caption">
-            <h2 class="home-h2">Check me out</h2>
-                <p>Text text text</p>
+            <h2 class="home-h2">Contact</h2>
+            <ul class="contact-list">
+            <li><i class="fa fa-star" aria-hidden="true"></i> Feel free to {{ HTML::mailto('enyakpalomino@gmai.com', 'send me an email') }}
+            </li>
+            <li>
+                <i class="fa fa-star" aria-hidden="true"></i> Check out my <a href="http://github.com/enyakarina">github</a>
+            </li>
+            <li>
+                <i class="fa fa-star" aria-hidden="true"></i> Send me a text @ <span class="popup-text">(210) 412-0292</span>
+            </li>
             </div>
         </div>
     </div>
