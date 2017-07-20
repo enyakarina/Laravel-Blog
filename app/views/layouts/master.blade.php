c0516f9b6e6296363082aa4bfa926017f09f088c
Karina <karina@alisons-MacBook-Air.local>
2017-07-20 18:57:54 -0500

    Added meta tag to homepage to verify ownership of domain in order to use google API

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="TMia6PiqWXTZpJ-zO1ST3mPlcIDBfKoNY0_LZI5UI88" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
